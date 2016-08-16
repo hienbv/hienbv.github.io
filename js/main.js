958cafddbfc52cdc828979f32b7ce1a944d960de
HienBV <hienbv@rikkeisoft.com>
2016-08-16 17:47:53 +0700

    update design

--- js/main.js
+++
@@ -1,9 +0,0 @@
-var app = angular.module('blog', []);
-app.controller('PostController', ['$scope', '$http', function ($scope, $http) {
-    $scope.posts = {};
-
-    $http.get('datasource/posts.json').then(function (response) {
-        $scope.posts = response.data;
-    }, function () {
-    });
-}]);
